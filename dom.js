a368bbce2c97dab92a11357e671753bc178a263c
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2023-10-31 11:11:46 +0530

    task 2

--- dom.js
+++ dom.js
@@ -1,3 +1,11 @@
 console.dir(document);
-document.title = 321
+const items = document.querySelectorAll(".list-group-item");
+
+if (items.length >= 3) {
+  items[2].style.backgroundColor = "green";
+}
+
+items.forEach((item) => {
+  item.style.fontWeight = "bold";
+});
 

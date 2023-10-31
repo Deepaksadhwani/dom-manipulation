414d72c89731a1a630b2fdb4abf4bb78d8136dcb
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2023-10-31 15:57:26 +0530

    added by idTag

--- dom.js
+++ dom.js
@@ -1,11 +1,29 @@
+console.log(document);
 console.dir(document);
-const items = document.querySelectorAll(".list-group-item");
 
-if (items.length >= 3) {
-  items[2].style.backgroundColor = "green";
+console.log(document.title);
+document.title = "Dom manipulation";
+
+console.log(document.all);
+console.log(document.all[4]);
+
+console.log(document.getElementById("title"));
+
+var content = document.getElementById("header-title");
+content.innerHTML = "<h3>change the title</h3>";
+content.style.border = "1px solid black";
+
+var addItem = document.getElementById("main");
+console.log(addItem.childNodes[1]);
+addItem.childNodes[1].style.fontWeight = "bold";
+addItem.childNodes[1].style.color = "green";
+
+var items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor = "green";
+
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight = "bold";
 }
 
-items.forEach((item) => {
-  item.style.fontWeight = "bold";
-});
-
+var li = document.getElementsByTagName("li");
+console.log(li[4].innerText);

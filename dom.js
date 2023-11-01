869cbc0edfbc93fc75d0b7f9a5d755d3053cf8fc
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2023-11-01 16:08:07 +0530

    added node properties in dom

--- dom.js
+++ dom.js
@@ -40,3 +40,70 @@ for (let i = 0; i < odd.length; i++) {
   odd[i].style.backgroundColor = "green";
   even[i].style.backgroundColor = "white";
 }
+
+// travelsing  the dom //
+var itemList = document.querySelector('#items')
+// parentNode 
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = "#f4f4f4";
+
+//childNode 
+console.log(itemList.childNodes);
+
+//childrenNode
+console.log(itemList.children);
+
+
+//first child 
+console.log(itemList.firstChild);
+
+// first element child
+console.log(itemList.firstElementChild);
+
+// last child 
+console.log(itemList.lastChild);
+
+//last element child
+console.log(itemList.lastElementChild);
+
+//next sibiling 
+console.log(itemList.nextSibling);
+
+// next sibiling element
+console.log(itemList.nextElementSibling);
+
+// previous sibiling element 
+console.log(itemList.previousSibling);
+
+// previous element sibling 
+console.log(itemList.previousElementSibling)
+
+//create element
+var newdiv = document.createElement("div");
+
+// class name
+newdiv.className = "hello";
+
+// set attribute 
+newdiv.setAttribute("title", "hello div");
+
+// creating Text node
+var newDivText = document.createTextNode('hello world')
+
+// append child
+newdiv.append(newDivText)
+
+var container = document.querySelector(".container");
+var h2 = document.querySelector('h1');
+
+container.insertBefore(newdiv,h2);
+
+//adding text before item 1
+
+var newItem = document.createElement("li");
+newItem.className = "list-group-item";
+var text2 = document.createTextNode("Hello world");
+newItem.appendChild(text2);
+console.log(newItem);
+
+container.insertBefore(newItem,h2);
